f0f677d47ac3d0ad68c51ed031c545a374bb92b1
Vishwa1112 <boxervishwa1@gmail.com>
2025-07-19 18:10:12 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,12 @@
+class BMW():
+    def brand(self):
+        print("Its a BMW ")
+
+class Ferrari():
+    def brand(self):
+        print("Its a Ferrari")
+
+b=BMW
+b.brand()
+f=Ferrari()
+f.brand()
