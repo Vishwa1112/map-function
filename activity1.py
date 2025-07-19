1797bcc6e1152b0bf86f871bc0074565f51e7b3e
Vishwa1112 <boxervishwa1@gmail.com>
2025-07-19 18:12:26 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,9 +1,21 @@
-class s():
-    def __init__(self,name):
-        self.name=name
-    def display(self):
-        print("Reverse String is ",self.name[::-1])
-name=input("Enter any string")
+class Person( object ):
 
-st=s(name)
-st.display()
+    def __init__(self,name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self):
+                print(self.name)
+                print(self.idnumber)
+
+class Employee( Person ):
+                def __init__(self, name, idnumber,salary,post):
+                            self.salary = salary
+                            self.post = post
+                            Person.__init__(self, name, idnumber)
+
+a = Employee('Rahul',8886012, 200000, "Intern")
+
+a.display()
+
+                            
